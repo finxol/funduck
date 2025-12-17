e9a3f5cf320843b613e410a4bb7a2714d57d3d43
finxol <git@finxol.io>
2025-12-17 15:20:34 +0100

    chore: remove singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite"
 import { VitePWA } from "vite-plugin-pwa"
-import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
     plugins: [
         VitePWA({
             registerType: "autoUpdate"
         }),
-        viteSingleFile({
-            removeViteModuleLoader: false,
-            useRecommendedBuildConfig: true,
-            inlinePattern: ["**/*.css"]
-        })
     ]
 })
