11299b161ed54b4ffda9a95f1b3a3b72bedd41b9
finxol <git@finxol.io>
2025-12-17 15:04:45 +0100

    feat: make it a single html file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite"
 import { VitePWA } from "vite-plugin-pwa"
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
     plugins: [
         VitePWA({
             registerType: "autoUpdate"
-        })
+        }),
+        viteSingleFile()
     ]
 })
